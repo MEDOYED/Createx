35f00e3ae9a7bfdc78ec874ef11a23780583443e
MEDOYED <mmax26072005@gmail.com>
2024-04-16 14:16:03 +0200

    section program

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function () {
 
-  var mixer = mixitup( '.directions__list' );
+  var mixer = mixitup('.directions__list');
 
   $('.directions__filter-btn').on('click', function () {
     $('.directions__filter-btn').removeClass('directions__filter-btn--active')
@@ -16,17 +16,17 @@ $(function () {
     waitForAnimate: false,
   })
 
-  $('.team__slider-prev').on('click', function(e) {
+  $('.team__slider-prev').on('click', function (e) {
     e.preventDefault()
     $('.team__slider').slick('slickPrev')
   })
 
-  $('.team__slider-next').on('click', function(e) {
+  $('.team__slider-next').on('click', function (e) {
     e.preventDefault()
     $('.team__slider').slick('slickNext')
   })
 
-  
+
   $('.testimonials__slider').slick({
     arrows: false,
     dots: true,
@@ -34,16 +34,56 @@ $(function () {
     waitForAnimate: false,
   })
 
-  $('.testimonials__prew').on('click', function(e) {
+  $('.testimonials__prew').on('click', function (e) {
     e.preventDefault()
     $('.testimonials__slider').slick('slickPrev')
   })
 
-  $('.testimonials__next').on('click', function(e) {
+  $('.testimonials__next').on('click', function (e) {
     e.preventDefault()
     $('.testimonials__slider').slick('slickNext')
   })
 
 
+  // accordion first version
+  // $('.program__accordeon-link').on('click', function(e) {
+  //   e.preventDefault()
+  //   $(this).toggleClass('program__accordeon-link--active')
+  //   $(this).children('.program__accordeon-text').slideToggle()
+  // })
 
-})
+
+  // accordion second version
+    $('.program__accordeon-link').on('click', function (e) {
+      e.preventDefault()
+      if ($(this).hasClass('program__accordeon-link--active')) {
+        $(this).removeClass('program__accordeon-link--active')
+        $(this).children('.program__accordeon-text').slideUp()
+      } else {
+        $('.program__accordeon-link').removeClass('program__accordeon-link--active')
+        $('.program__accordeon-text').slideUp()
+        $(this).addClass('program__accordeon-link--active')
+        $(this).children('.program__accordeon-text').slideDown()
+      }
+    })
+  })
+
+
+
+    //accordion third version
+//   $('.program__accordeon-link').on('click', function (e) {
+//     e.preventDefault();
+
+//     var isActive = $(this).hasClass('program__accordeon-link--active');
+
+//     // Зняття активного класу та згортання всіх текстів
+//     $('.program__accordeon-link').removeClass('program__accordeon-link--active');
+//     $('.program__accordeon-text').slideUp();
+
+//     // Якщо елемент не активний - розгорнути
+//     if (!isActive) {
+//       $(this).addClass('program__accordeon-link--active');
+//       $(this).children('.program__accordeon-text').slideDown();
+//     }
+//   });
+// })
